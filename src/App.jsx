f1491d5541d98486298e7b681dfdd6f4bd97c9af
alejoT <yo.alejo.at@gmail.com>
2024-02-23 21:38:00 -0300

    Configuración de sección menu desplegable

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
-            <button onClick={() => toggleMenu()} className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:bg-gray-700 text-white">
+            <button onClick={() => toggleMenu()} className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:bg-gray-700 text-white opacity-100">
               <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
@@ -99,12 +99,12 @@ function App() {
 
         </div>
         {isOpen && (
-        <div className="md:hidden z-50 fixed top-[60px] right-[30px] rounded-tl-xl rounded-br-xl bg-gray-700">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="#about" onClick={(e) => {handleClick(e, 'about'); toggleMenu()}} className="block px-3 py-2 rounded-md text-base font-medium text-white">Acerca de Mi</a>
-            <a href="#habilidades" onClick={(e) => {handleClick(e, 'habilidades'); toggleMenu()}} className="block px-3 py-2 rounded-md text-base font-medium text-white ">Tecnologías</a>
-            <a href="#proyectos" onClick={(e) => {handleClick(e, 'proyectos'); toggleMenu()}} className="block px-3 py-2 rounded-md text-base font-medium text-white ">Proyecctos</a>
-            <a href="#contacto" onClick={(e) => {handleClick(e, 'contacto'); toggleMenu()}} className="block px-3 py-2 rounded-md text-base font-medium text-white ">Contactos</a>
+        <div className="md:hidden z-50 fixed top-[60px] right-[25px] rounded-tl-xl rounded-br-xl bg-gray-700 px-3">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
+            <a href="#about" onClick={(e) => {handleClick(e, 'about'); toggleMenu()}} className="font-bold block px-3 py-2  text-base text-white">Acerca de Mi</a>
+            <a href="#habilidades" onClick={(e) => {handleClick(e, 'habilidades'); toggleMenu()}} className="font-bold block px-3 py-2text-base text-white ">Tecnologías</a>
+            <a href="#proyectos" onClick={(e) => {handleClick(e, 'proyectos'); toggleMenu()}} className="font-bold block px-3 py-2 text-base text-white ">Proyecctos</a>
+            <a href="#contacto" onClick={(e) => {handleClick(e, 'contacto'); toggleMenu()}} className="font-bold block px-3 py-2 text-base text-white ">Contactos</a>
           </div> 
         </div>
       )}
@@ -136,7 +136,7 @@ function App() {
         </div>
       </div>
       <div id='about' className='p-6 md:mt-5 mt-3 flex justify-center'>
-        <div className={`border-white shadow-white shadow-[8px_5px_11px_-1px_rgba(255,255,255)]  border-4 w-[54rem] text-white pt-8 md:pt-16 px-6 md:px-16 pb-10 rounded-2xl ${valorScroll > 280 ? ' opacity-100 duration-1000':'translate-y-[15rem] opacity-0 duration-700'}`}>
+        <div className={`border-white shadow-white shadow-[8px_5px_11px_-1px_rgba(255,255,255)]  border-4 w-[54rem] text-white pt-8 md:pt-16 px-7 md:px-16 pb-10 rounded-2xl ${valorScroll > 280 ? ' opacity-100 duration-1000':'translate-y-[15rem] opacity-0 duration-700'}`}>
           <h2 className='text-3xl md:text-5xl pb-3'>Sobre mi</h2>
           <hr/>
           <div className='md:mt-4 mt-2 text-lg text-justify italic'>
